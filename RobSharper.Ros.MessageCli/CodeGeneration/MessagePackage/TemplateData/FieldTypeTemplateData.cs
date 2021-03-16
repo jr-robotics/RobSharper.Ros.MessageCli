96055e643f94f2a96ce0d4d551adbdc707252435
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2021-03-16 17:24:44 +0100

    Add property initialization for lists and duration

--- RobSharper.Ros.MessageCli/CodeGeneration/MessagePackage/TemplateData/FieldTypeTemplateData.cs
+++ RobSharper.Ros.MessageCli/CodeGeneration/MessagePackage/TemplateData/FieldTypeTemplateData.cs
@@ -10,14 +10,20 @@ namespace RobSharper.Ros.MessageCli.CodeGeneration.MessagePackage.TemplateData
         public string ConcreteName { get; set; }
 
         public bool IsBuiltInType => TypeInfo.IsBuiltInType;
+        
+        
         public bool IsArray => TypeInfo.IsArray;
+        public int ArraySize => TypeInfo.ArraySize;
+        public bool IsVariableSizeArray => IsArray && ArraySize == 0;
+        public bool IsFixedSizeArray => IsArray && ArraySize > 0;
+        
 
         public bool IsValueType => TypeInfo.IsValueType();
         public bool SupportsEqualityComparer => TypeInfo.SupportsEqualityComparer();
         public bool IsString => TypeInfo.IsType<string>();
+        
         public bool IsDateTime => TypeInfo.IsType<DateTime>();
         public bool IsTimeSpan => TypeInfo.IsType<TimeSpan>();
-
         public bool IsDateTimeOrTimeSpan => IsDateTime || IsTimeSpan;
 
         public FieldTypeTemplateData(string interfaceName, string concreteName, RosTypeInfo typeInfo)
